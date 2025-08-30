2620688e5017062e2a3ae70d4b86f9aca0dcaa10
Test project <Jtanjun@gmail.com>
2025-08-30 19:00:13 -0400

    Commit

---
+++ Python File.py
@@ -0,0 +1,16 @@
+x=5
+y=6
+print("The answer to the query:"+ str(x*y))
+
+w=8
+i=3
+print("The answer to the query:"+ str(w/i))
+print("The answer to the query:"+ str(w//i))
+print("The answer to the query:"+ str(w % i))
+
+r=4200000
+f=23090
+print("The answer to the query:"+ str(r+f))
+
+t=7633
+print("The answer to the query:"+ str(r-t))
